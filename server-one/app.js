3ba2d6c3e723b5de4a96de80fb6940f078ce01b5
R <ramz9_97@hotmail.com>
2021-10-29 01:04:31 +0300

    cleaned code

--- server-one/app.js
+++ server-one/app.js
@@ -4,8 +4,6 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
-const path = require('path');
-//app.use('/public', express.static(path.join(__dirname, 'public')))
 app.use(express.static('public'));
 
 app.get('/', (req, res) => {
